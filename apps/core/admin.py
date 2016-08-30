f607ac06b9bff9e6c41bba016ddd2401b920a984
naerthon <naerthon@gmail.com>
2016-08-30 18:27:23 -0300

    update

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -4,19 +4,22 @@ from .models import Aluno, Curso, Aula, Turma, Frequencia, Situacao
 # Register your models here.
 
 class AlunoAdmin(admin.ModelAdmin):
-        fields = ('nome', 'cidade', 'estado','bairro', 'data_nascimento', 'fone', 'whatsapp','data_criacao','data_atualizacao',)
+    fields = ('nome', 'cidade', 'estado','bairro', 'data_nascimento', 'fone', 'whatsapp','data_criacao','data_atualizacao',)
 
 class AulaAdmin(admin.ModelAdmin):
-        fields = ('aula','turma', 'data')
+    fields = ('aula','turma', 'data')
+    list_filter = ('turma',)
+    date_hierarchy = 'data'
 
 class TurmaAdmin(admin.ModelAdmin):
-        fields = ('turma', 'curso','aluno', 'instrutor', 'limite_faltas', 'data_inicio', 'data_termino','ativo')
+    fields = ('turma', 'curso','aluno', 'instrutor', 'limite_faltas', 'data_inicio', 'data_termino','ativo')
+
+class FrequenciaAdmin(admin.ModelAdmin):
+    list_display = ('aula', 'aluno', 'frequencia')
 
 admin.site.register(Aluno, AlunoAdmin)
 admin.site.register(Aula, AulaAdmin)
 admin.site.register(Turma, TurmaAdmin)
-admin.site.register(Situacao)
+#admin.site.register(Frequencia, FrequenciaAdmin)
+#admin.site.register(Situacao)
 admin.site.register(Curso)
-@admin.register(Frequencia)
-class FrequenciaAdmin(admin.ModelAdmin):
-    list_display = ('aula', 'aluno', 'frequencia')
